90646e8ce727eedc8a6cf8062a1b8870cc6a000d
git <>
2021-06-14 17:57:06 +0900

    モデルファイル作成

---
+++ app/models/mytweet.rb
@@ -0,0 +1,2 @@
+class Mytweet < ApplicationRecord
+end
---
+++ test/models/mytweet_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MytweetTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
