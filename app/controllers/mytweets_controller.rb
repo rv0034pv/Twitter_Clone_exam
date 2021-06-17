86fb47255919e6ec02c855265e072fd91b229170
git <>
2021-06-17 21:48:47 +0900

    詳細確認機能追加

--- app/controllers/mytweets_controller.rb
+++ app/controllers/mytweets_controller.rb
@@ -1,14 +1,32 @@
 class MytweetsController < ApplicationController
   def new
-    
+    @mytweet = Mytweet.new
+  end
+
+  def create
+    @mytweet = Mytweet.create(mytweet_params)
+    if @mytweet.save
+      # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
+      redirect_to mytweets_path, notice: "ツイートに成功しました！"
+    else
+      # 入力フォームを再描画します。
+      render :new
+    end
   end
 
   def edit
   end
 
   def index
+    @mytweets = Mytweet.all
   end
 
   def show
+  @mytweet = Mytweet.find(params[:id])
+  end
+
+  private
+  def mytweet_params
+    params.require(:mytweet).permit(:content)
   end
 end
--- app/models/mytweet.rb
+++ app/models/mytweet.rb
@@ -1,2 +1,4 @@
 class Mytweet < ApplicationRecord
+  validates :content, length: { in: 1..140 }
+  validates :content, presence: true
 end
